b7854fb3410e67acc29a7361ed1d3d1fede2686b
YoungLeePro <879605552@qq.com>
2021-08-19 23:55:16 +0800

    feat: 调整

---
+++ springboot-distribution/src/main/java/com/yt/springbootdistribution/SpringbootDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.yt.springbootdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDistributionApplication.class, args);
+    }
+
+}
---
+++ springboot-distribution/src/test/java/com/yt/springbootdistribution/SpringbootDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.springbootdistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
